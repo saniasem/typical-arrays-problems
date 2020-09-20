8441ddefdd71af56dda2e88c651f0bc80fd294ae
Oleksandr Semeniuk <olexandr.semenyuk@hotmail.com>
2020-09-20 11:19:09 +0300

    feat: implement functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == null || array.length === 0) {
+        return 0;
+    }
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == null || array.length === 0) {
+        return 0;
+    }
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == null || array.length === 0) {
+        return 0;
+    }
+    return array.reduce((acc, curr) => {
+        return acc + curr
+    }, 0) / array.length;
+
 }
